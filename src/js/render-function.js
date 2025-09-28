df2773fe57cd6de11b71580ae6bce85440320669
Tetiana Kalkuta <72383361+TanyaKalkuta@users.noreply.github.com>
2025-09-28 14:30:09 +0100

    products items

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -13,3 +13,21 @@ export function renderCategories(categories) {
     .join('');
   refs.elCategories.insertAdjacentHTML('beforeend', markup);
 }
+
+function murkupProdect(products) {
+  return products
+    .map(
+      ({ id, thumbnail, title, brand, category, price }) => `
+    <li class="products__item" data-id="${id}">
+    <img class="products__image" src="${thumbnail}" alt="${title}"/>
+    <p class="products__title">${title}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:${brand}</span></p>
+    <p class="products__category">Category:${category} </p>
+    <p class="products__price">Price: ${price}$</p>
+ </li>`
+    )
+    .join('');
+}
+export function renderProduct(products) {
+  refs.elProducts.insertAdjacentHTML('beforeend', murkupProdect(products));
+}
