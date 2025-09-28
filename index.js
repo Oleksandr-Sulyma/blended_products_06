6e1e06abff4e72082ac160fb4f5c0d660f974a3c
Oleksandr-Sulyma <sulima.ab@gmail.com>
2025-09-28 18:14:18 +0000

    Deploying to gh-pages from @ Oleksandr-Sulyma/blended_products_06@ca210897b580eb42f8b1329c08d4c97423f3e523 🚀

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-import"./assets/styles-JE8YjOlG.js";import{a as u}from"./assets/vendor-N5iQpiFS.js";const o=12,c=u.create({baseURL:"https://dummyjson.com/products"}),d=async(t=1)=>{try{return(await c.get(`?limit=${o}&skip=${(t-1)*o}&select=id,thumbnail,title,brand,category,price`)).data.products}catch(e){throw e}},l=async()=>{try{return(await c.get("/category-list")).data}catch(t){throw t}},a={elCategories:document.querySelector(".categories"),elProducts:document.querySelector(".products")};function m(t){t.unshift("All");const e=t.map(s=>`
+import"./assets/styles-JE8YjOlG.js";import{a as p}from"./assets/vendor-N5iQpiFS.js";const o=12,n=p.create({baseURL:"https://dummyjson.com/products"}),i=async(t=1)=>{try{return(await n.get(`?limit=${o}&skip=${(t-1)*o}&select=id,thumbnail,title,brand,category,price`)).data.products}catch(e){throw e}},g=async()=>{try{return(await n.get("/category-list")).data}catch(t){throw t}},_=async(t,e=1)=>{try{return(await n.get(`/category/${t}?limit=${o}&skip=${(e-1)*o}&select=id,thumbnail,title,brand,category,price`)).data.products}catch(s){throw s}},c={elCategories:document.querySelector(".categories"),elProducts:document.querySelector(".products")};function m(t){t.unshift("All");const e=t.map(s=>`
      <li class="categories__item">
-   <button class="categories__btn" type="button">${s}</button>
- </li>   
-        `).join("");a.elCategories.insertAdjacentHTML("beforeend",e)}function g(t){return t.map(({id:e,thumbnail:s,title:r,brand:n,category:i,price:p})=>`
+      <button class="categories__btn ${s==="All"?"categories__btn--active":""}" type="button">${s}</button>
+    </li>`).join("");c.elCategories.insertAdjacentHTML("beforeend",e)}function y(t){return t.map(({id:e,thumbnail:s,title:r,brand:l,category:u,price:d})=>`
     <li class="products__item" data-id="${e}">
     <img class="products__image" src="${s}" alt="${r}"/>
     <p class="products__title">${r}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand:${n}</span></p>
-    <p class="products__category">Category:${i} </p>
-    <p class="products__price">Price: ${p}$</p>
- </li>`).join("")}function _(t){a.elProducts.insertAdjacentHTML("beforeend",g(t))}async function y(){try{const t=await l();m(t);const e=await d();_(e)}catch(t){console.log(t.message)}}y();
+    <p class="products__brand"><span class="products__brand--bold">Brand:${l}</span></p>
+    <p class="products__category">Category:${u} </p>
+    <p class="products__price">Price: ${d}$</p>
+ </li>`).join("")}function a(t){c.elProducts.insertAdjacentHTML("beforeend",y(t))}async function b(){try{const t=await g();m(t);const e=await i();a(e)}catch(t){console.log(t.message)}}b();c.elCategories.addEventListener("click",$);async function $(t){if(t.preventDefault(),!t.target.classList.contains("categories__btn"))return;t.currentTarget.querySelector(".categories__btn--active").classList.remove("categories__btn--active");const e=t.target.closest(".categories__btn"),s=e.textContent.trim().toLowerCase();e.classList.add("categories__btn--active"),c.elProducts.innerHTML="";try{if(s!=="all"){const r=await _(s);a(r)}else{const r=await i();a(r)}}catch(r){console.log(r.message)}}
 //# sourceMappingURL=index.js.map
