be6a93ca5fd425303a1868b42b92e4b4fddf89c8
hog-seruj <hogseruj1@gmail.com>
2025-09-28 13:34:42 +0000

    Deploying to gh-pages from @ Oleksandr-Sulyma/blended_products_06@3375b9580f19c3e5df3d82fbd34bc1fbb6b328a2 🚀

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-import"./assets/styles-JE8YjOlG.js";import{a as s}from"./assets/vendor-N5iQpiFS.js";const r=s.create({baseURL:"https://dummyjson.com/products"}),n=async()=>{try{return(await r.get("/category-list")).data}catch(t){throw t}},c={elCategories:document.querySelector(".categories"),elProducts:document.querySelector(".products")};function a(t){t.unshift("All");const e=t.map(o=>`
+import"./assets/styles-JE8YjOlG.js";import{a as u}from"./assets/vendor-N5iQpiFS.js";const o=12,c=u.create({baseURL:"https://dummyjson.com/products"}),d=async(t=1)=>{try{return(await c.get(`?limit=${o}&skip=${(t-1)*o}&select=id,thumbnail,title,brand,category,price`)).data.products}catch(e){throw e}},l=async()=>{try{return(await c.get("/category-list")).data}catch(t){throw t}},a={elCategories:document.querySelector(".categories"),elProducts:document.querySelector(".products")};function m(t){t.unshift("All");const e=t.map(s=>`
      <li class="categories__item">
-   <button class="categories__btn" type="button">${o}</button>
+   <button class="categories__btn" type="button">${s}</button>
  </li>   
-        `).join("");c.elCategories.insertAdjacentHTML("beforeend",e)}async function i(){try{const t=await n();a(t)}catch(t){console.log(t.message)}}i();
+        `).join("");a.elCategories.insertAdjacentHTML("beforeend",e)}function g(t){return t.map(({id:e,thumbnail:s,title:r,brand:n,category:i,price:p})=>`
+    <li class="products__item" data-id="${e}">
+    <img class="products__image" src="${s}" alt="${r}"/>
+    <p class="products__title">${r}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:${n}</span></p>
+    <p class="products__category">Category:${i} </p>
+    <p class="products__price">Price: ${p}$</p>
+ </li>`).join("")}function _(t){a.elProducts.insertAdjacentHTML("beforeend",g(t))}async function y(){try{const t=await l();m(t);const e=await d();_(e)}catch(t){console.log(t.message)}}y();
 //# sourceMappingURL=index.js.map
