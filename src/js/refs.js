e53f705f30278f3651064f64c2c18c6ef3013e5d
Oleksandr Sulyma <sulima.ab@gmail.com>
2025-09-29 16:27:42 +0300

    maked task №4

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,6 @@
 export default {
     elCategories: document.querySelector('.categories'),
     elProducts: document.querySelector('.products'),
+    cardProduct: document.querySelector('.modal-product'),
+    modal: document.querySelector('.modal'),
 }
